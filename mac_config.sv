1a2f67043792e5025f982a1dee0360e53e4ef052
jchengX <496272507@qq.com>
2017-06-06 00:38:15 +0800

    Update mac_config.sv

--- mac_config.sv
+++ mac_config.sv
@@ -1,1 +1,11 @@
+class mac_config extends uvm_object;
 
+    `uvm_object_utils(mac_config)
+        virtual apb_if vif;
+  
+
+    function new(string name="mac_config");
+        super.new(name);
+    endfunction
+
+endclas
