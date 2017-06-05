28c26c2f29a5ea3fe9789d81ccb366fc4187ba6b
jchengX <496272507@qq.com>
2017-06-06 00:50:24 +0800

    Update mac_agent.sv

--- mac_agent.sv
+++ mac_agent.sv
@@ -1,37 +1,52 @@
-class crc7_agent extends uvm_agent;
-  `uvm_component_utils(crc7_agent)
-  
-  uvm_analysis_port #(crc7_transaction) agent_ap_before;
-  uvm_analysis_port #(crc7_transaction) agent_ap_after;
-  
-  crc7_sequencer c7_seqr;
-  crc7_driver c7_drvr;
-  crc7_monitor_before c7_mon_before;
-  crc7_monitor_after c7_mon_after;
-  
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction: new
-  
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
+////////////////////////////////////
+// File : mac_agent
+// Data : 2017/06/03
+// Description : mac bfm agent
+////////////////////////////////////
+
+`ifndef MAC_AGENT__SV
+`define MAC_AGENT__SV
+
+typedef class mac_agent;
+
+class mac_agent extends uvm_agent;
+
+    mac_config      cfg;
+    mac_sequencer   sqr;
+    mac_driver      drv;
+    mac_monitor     mon;
+
+    mac_if         vif;
+
+    `uvm_component_utils_begin(mac_agent)
+        `uvm_field_int(sqr_on,UVM_ALL_ON)
+        `uvm_field_int(drv_on,UVM_ALL_ON)
+        `uvm_field_int(mon_on,UVM_ALL_ON)
+        `uvm_field_object(cfg,UVM_ALL_ON)
+        `uvm_field_object(sqr,UVM_ALL_ON)
+        `uvm_field_object(drv,UVM_ALL_ON)
+        `uvm_field_object(mon,UVM_ALL_ON)
+    `uvm_component_utils_end
     
-    agent_ap_before = new(.name("agent_ap_before"), .parent(this));
-    agent_ap_after = new(.name("agent_ap_after"), .parent(this));
-    
-    c7_seqr = crc7_sequencer::type_id::create(.name("c7_seqr"), .parent(this));
-    c7_drvr = crc7_driver::type_id::create(.name("c7_drvr"), .parent(this));
-    c7_mon_before = crc7_monitor_before::type_id::create(.name("c7_mon_before"), .parent(this));
-    c7_mon_after = crc7_monitor_after::type_id::create(.name("c7_mon_after"), .parent(this));
-  endfunction: build_phase
-  
-  function void connect_phase(uvm_phase phase);
-    super.connect_phase(phase);
-    
-    c7_drvr.seq_item_port.connect(c7_seqr.seq_item_export);
-    c7_mon_before.mon_ap_before.connect(agent_ap_before);
-    c7_mon_after.mon_ap_after.connect(agent_ap_after);
-  endfunction: connect_phase
-endclass: crc7_agent
-    
-    
+    function new(string name, uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+
+    virtual function void build_phase(uvm_phase phase);
+
+        sqr = mac_sequencer::type_id::create("sqr", this);
+        drv = mac_driver::type_id::create("drv", this);
+        mon = mac_monitor::type_id::create("mon", this);
+
+        if (!uvm_config_db#(mac_vif)::get(this, "", "vif", vif)) begin
+            `uvm_fatal("mac_agent", "No virtual interface specified for this agent instance")
+        end
+    endfunction : build_phase
+
+    virtual function void connect_phase(uvm_phase phase);
+        if((drv_on==1'b1)&&(sqr_on==1'b1)) begin
+            drv.seq_item_port.connect(sqr.seq_item_export);
+        end
+    endfunction : connect_phase
+endclass: mac_agent
+`endif // MAC_AGENT__SV    
