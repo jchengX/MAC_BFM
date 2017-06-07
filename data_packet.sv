4990e9e489ed6784d219dee73440d1d8be66eae2
jchengX <496272507@qq.com>
2017-06-07 16:48:47 +0800

    Update data_packet.sv

--- data_packet.sv
+++ data_packet.sv
@@ -1,1 +1,8 @@
-class data_packet
+class unicast_packet extends packet;
+
+  function agent_unicast_packet(output logic [PACKET_LEN-1:0] agt_unicast_packet);
+	  type = 2'b10;
+	  sub_type = 4'b0000;
+	  agent_packet(agt_unicast_packet);
+  endfunction
+  endclass
